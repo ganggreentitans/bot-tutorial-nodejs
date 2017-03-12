6fedcbbb85f4e4177e143bbe1a5fc60cc9e9c02a
ganggreentitans <ganggreentitans@gmail.com>
2017-03-11 18:10:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
       botRegexAd=/^\/advance/;botRegexGTA = /^\/benice/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/superbowl/; botRegexSh = /^\/shrug/; botRegexWk = /^\/contacts/;
       botRegexRu = /^\/rules/; botRegexYu = /^\/youtube/i; botRegexSc = /^\/schedule/; botRegexSt = /^\/standings/; botRegexFo = /^\/forum/;
-      botRegexGoat = /^\/goat/; botRegexZach = /^\/lyin/; botRegexHogan = /^\/boot/i; botRegexHogan2 = /(.*\s+)(.*boot)(\s+.*)/i;
+      botRegexGoat = /^\/goat/; botRegexZach = /^\/lyin/; botRegexHogan = /^\/boot/; botRegexHogan2 = /(.*\s+)(.*boot)(\s+.*)/i;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
