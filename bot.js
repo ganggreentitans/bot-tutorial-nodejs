5474022b34a8cc2d3cbd641ce5e01a81d478acc8
ganggreentitans <ganggreentitans@gmail.com>
2017-03-11 18:06:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
       botRegexAd=/^\/advance/;botRegexGTA = /^\/benice/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/superbowl/; botRegexSh = /^\/shrug/; botRegexWk = /^\/contacts/;
       botRegexRu = /^\/rules/; botRegexYu = /^\/youtube/i; botRegexSc = /^\/schedule/; botRegexSt = /^\/standings/; botRegexFo = /^\/forum/;
-      botRegexGoat = /^\/goat/; botRegexZach = /^\/lyin/; botRegexHogan = /^\boot/i;
+      botRegexGoat = /^\/goat/; botRegexZach = /^\/lyin/; botRegexHogan = /^\/boot/i; botRegexHogan2 = /(.*\s+)(.*boot)(\s+.*)/i;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -122,6 +122,11 @@ function respond() {
     this.res.end();
   }
          else if(request.text && botRegexHogan.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.giphy.com/11MhU7NWOigjdK.gif");
+    this.res.end();
+  }
+          else if(request.text && botRegexHogan2.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://i.giphy.com/11MhU7NWOigjdK.gif");
     this.res.end();
